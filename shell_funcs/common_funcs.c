60a61e3e4b3ad8bdfa5cc13fbf88bd41ec3d5970
Parth <frostgameing0@gmail.com>
2024-10-02 00:50:25 +0530

    added a new function for future usage

--- shell_funcs/common_funcs.c
+++ shell_funcs/common_funcs.c
@@ -15,3 +15,69 @@ void display_author()
 {
     printf("<-> Shadow Shell (Version : %s, Author : Parth)\n", VERSION );
 }
+
+wchar_t **tokenize(wchar_t *input, int *tokens, int *max_token_len)
+{
+    *tokens = 0;
+    *max_token_len = 0;
+    int token_len = 0;
+
+    int input_len = wcslen(input);
+
+    for (int i = 0; i < input_len; i++)
+    {
+        if (!isspace(input[i]))
+            token_len++;
+        else
+        {
+            if (token_len > *max_token_len)
+                *max_token_len = token_len;
+            token_len = 0;
+        }
+
+        if ((i == 0 || isspace(input[i])) && !isspace(input[i - 1]))
+            (*tokens)++;
+        if (i == input_len - 1 && isspace(input[input_len - 1]))
+            (*tokens)--;
+    }
+
+    wchar_t **output = (wchar_t **)malloc(*tokens * sizeof(wchar_t*));
+    if (output == NULL)
+        display_error("Insufficient memory.");
+
+    for (int i = 0; i < *tokens; i++)
+    {
+        output[i] = (wchar_t *)malloc((*max_token_len + 1)*sizeof(wchar_t));
+        if (output[i] == NULL)
+        {
+            display_error("Insufficient memory.");
+            for (int j = 0; j <= i; j++)
+                free(output[i]);
+            free(output);
+        }
+    }
+
+    int notspace = 0;
+    for (int i = 0, j = 0, k = 0; i < input_len; i++)
+    {
+        if(!isspace(input[i]))
+        {
+            output[j][k] = input[i];
+            notspace = 1;
+            k++;
+        }
+        else
+        {
+            if(notspace == 1)
+            {
+                output[j][k] = '\0';
+                j++;
+            }
+            else if(i == input_len-1)
+                break;
+            notspace = k = 0;
+        }
+    }
+
+    return output;
+}
--- shell_funcs/common_funcs.h
+++ shell_funcs/common_funcs.h
@@ -4,3 +4,4 @@
 void display_error(wchar_t* error);
 void quit();
 void display_author();
+wchar_t **tokenize(wchar_t *input, int *tokens, int *max_token_len);
